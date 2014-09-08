22610b006af8dc637168a16436a3bfcd2bfe865d
Thomas Merkel <tm@core.io>
2014-09-08 22:36:42 +0200

    add contact page

--- web.py
+++ web.py
@@ -14,5 +14,9 @@ def show_result():
 	searcher = RBLSearch(request.form['ip'])
 	return render_template('result.html', results=searcher.listed)
 
+@app.route('/contact/')
+def show_contact():
+	return render_template('contact.html')
+
 if __name__ == '__main__':
 	app.run()
